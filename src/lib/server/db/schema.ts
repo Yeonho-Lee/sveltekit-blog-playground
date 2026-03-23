66f7cd5235f73f9e69a55f9fc47e37d2cfbcdcff
Lee Yeon Ho <55523393+Yeonho-Lee@users.noreply.github.com>
2026-03-23 22:48:25 +0900

    post: 2026-03-16-한글로 DB 테이블/컬럼명을 지정할 수 있다고? - Drizzle ORM

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -7,3 +7,9 @@ export const task = sqliteTable('task', {
   title: text('title').notNull(),
   priority: integer('priority').notNull().default(1)
 });
+
+export const 학생 = sqliteTable('학생', {
+  아이디: integer('아이디').primaryKey({ autoIncrement: true }),
+  이름: text('이름').notNull(),
+  학번: text('학번').notNull().unique()
+});
